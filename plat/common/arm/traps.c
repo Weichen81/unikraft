4bff4903e465fdada7802e6b327b57a70f62e141
Wei Chen <wei.chen@arm.com>
2018-09-20 15:17:07 +0000

    plat/common: Add a trap function to handle Arm64 irq trap
    Signed-off-by: Wei Chen <wei.chen@arm.com>

--- plat/common/arm/traps.c
+++ plat/common/arm/traps.c
@@ -23,6 +23,7 @@
 #include <string.h>
 #include <uk/print.h>
 #include <uk/assert.h>
+#include <arm/gic-v2.h>
 
 static const char *exception_modes[]= {
 	"Synchronous Abort",
@@ -67,4 +68,28 @@ void trap_el1_sync(struct __regs *regs, uint64_t far)
 
 	dump_registers(regs, far);
 	ukplat_crash();
+}
+
+void trap_el1_irq(struct __regs *regs, uint64_t far)
+{
+	uint32_t stat, irq;
+
+	do {
+		stat = gic_ack_irq();
+		irq = stat & GICC_IAR_INTID_MASK;
+
+		uk_printd(DLVL_CRIT, "Unikraft: EL1 IRQ#%d trap caught\n", irq);
+
+		/*
+		 * TODO: Hanle IPI&SGI interrupts here
+		 */
+		if (irq < GIC_MAX_IRQ) {
+			isb();
+			_ukplat_irq_handle((unsigned long)irq);
+			gic_eoi_irq(stat);
+			continue;
+		}
+
+		break;
+	} while (1);
 }
