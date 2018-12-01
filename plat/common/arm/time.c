730039bf6b284e3a78147075c6001e8b7f1b01e6
Wei Chen <wei.chen@arm.com>
2018-12-01 15:51:52 +0000

    plat/common: Add arm64 timer API for scheduler
    Change-Id: I0abbffee154aaba626175b27b03d9089e142283a
    Signed-off-by: Wei Chen <wei.chen@arm.com>

--- plat/common/arm/time.c
+++ plat/common/arm/time.c
@@ -35,56 +35,151 @@
 #include <libfdt.h>
 #include <uk/assert.h>
 #include <uk/plat/time.h>
+#include <uk/plat/lcpu.h>
 #include <uk/plat/irq.h>
 #include <uk/bitops.h>
 #include <cpu.h>
+
+/* Bits definition of cntv_ctl_el0 register */
+#define GT_TIMER_ENABLE		0x01
+#define GT_TIMER_MASK_IRQ	0x02
+#define GT_TIMER_IRQ_STATUS	0x04
 
 static uint64_t boot_ticks;
 static uint32_t counter_freq;
 extern void *_libkvmplat_dtb;
 
-/*
- * Shift factor for counter scaling multiplier; referred to as S in the
- * following comments.
- */
-static uint8_t counter_shift;
 
-/* Multiplier for converting counter ticks to nsecs. (0.S) fixed point. */
+/* Shift factor for converting ticks to ns */
+static uint8_t counter_shift_to_ns;
+
+/* Shift factor for converting ns to ticks */
+static uint8_t counter_shift_to_tick;
+
+/* Multiplier for converting counter ticks to nsecs */
 static uint32_t ns_per_tick;
+
+/* Multiplier for converting nsecs to counter ticks */
+static uint32_t tick_per_ns;
+
+/*
+ * The maximum time range in seconds which can be converted by multiplier
+ * and shift factors. This will guarantee the converted value not to exceed
+ * 64-bit unsigned integer. Increase the time range will reduce the accuracy
+ * of conversion, because we will get smaller multiplier and shift factors.
+ * In this case, we selected 3600s as the time range.
+ */
+#define __MAX_CONVERT_SECS	3600UL
+
+/*
+ * Minimum delta to sleep using generic timer.
+ */
+static uint32_t counter_mini_delta;
 
 /* How many nanoseconds per second */
 #define NSEC_PER_SEC ukarch_time_sec_to_nsec(1)
 
 static inline uint64_t ticks_to_ns(uint64_t ticks)
 {
-	return (ns_per_tick * ticks) >> counter_shift;
+	return (ns_per_tick * ticks) >> counter_shift_to_ns;
+}
+
+static inline uint64_t ns_to_ticks(uint64_t ns)
+{
+	return (tick_per_ns * ns) >> counter_shift_to_tick;
 }
 
 /*
- * On a few platforms the frequency is not configured correctly
- * by the firmware. A property in the DT (clock-frequency) has
- * been introduced to workaround those firmware. So, we will try
- * to get clock-frequency from DT first, if failed we will read
- * the register directly.
+ * Calculate multiplier/shift factors for scaled math.
  */
-static uint32_t get_counter_frequency(void)
+static void calculate_mult_shift(uint32_t *pmult, uint8_t *pshift,
+		uint64_t source, uint64_t target)
 {
-	int fdt_archtimer, len;
-	const uint64_t *fdt_freq;
+	uint64_t tmp;
+	uint32_t mult;
+	uint8_t shift = 32;
+
+	/*
+	 * Get the maximum shift factor (max_shift) for the given
+	 * conversion range.
+	 */
+	tmp = (__MAX_CONVERT_SECS * (uint64_t)source) >> shift;
+	while (tmp) {
+		tmp >>=1;
+		shift--;
+	}
+
+	/*
+	 * Calculate shift factor (S) and scaling multiplier (M).
+	 *
+	 * (S) needs to be the largest shift factor (<= max_shift) where
+	 * the result of the M calculation below fits into uint32_t
+	 * without truncation.
+	 *
+	 * multiplier = (target << shift) / source
+	 */
+	mult = 0;
+	do {
+		tmp = ((uint64_t)target << shift) / source;
+		if ((tmp & 0xFFFFFFFF00000000L) == 0L)
+			mult = (uint32_t)tmp;
+		else
+			shift--;
+	} while (shift > 0 && mult == 0L);
+
+	*pmult = mult;
+	*pshift = shift;
+}
+
+static inline void generic_timer_enable(void)
+{
+	SYSREG_WRITE32(cntv_ctl_el0, GT_TIMER_ENABLE);
+}
+
+static inline void generic_timer_mask_irq(void)
+{
+	SYSREG_WRITE32(cntv_ctl_el0,
+		SYSREG_READ32(cntv_ctl_el0) | GT_TIMER_MASK_IRQ);
+}
+
+static inline void generic_timer_unmask_irq(void)
+{
+	SYSREG_WRITE32(cntv_ctl_el0,
+		SYSREG_READ32(cntv_ctl_el0) & (~GT_TIMER_MASK_IRQ));
+}
+
+static inline void generic_timer_update_compare(uint64_t new_val)
+{
+	SYSREG_WRITE64(cntv_cval_el0, new_val);
+}
+
+static int generic_timer_get_fdt_offset(void)
+{
+	int fdt_timer;
 
 	/* Try to find arm,armv8-timer first */
-	fdt_archtimer = fdt_node_offset_by_compatible(_libkvmplat_dtb,
+	fdt_timer = fdt_node_offset_by_compatible(_libkvmplat_dtb,
 						-1, "arm,armv8-timer");
 	/* If failed, try to find arm,armv7-timer */
-	if (fdt_archtimer < 0)
-		fdt_archtimer = fdt_node_offset_by_compatible(_libkvmplat_dtb,
-							-1, "arm,armv7-timer");
-	/* DT doesn't provide arch timer information */
-	if (fdt_archtimer < 0)
-		goto endnofreq;
+	if (fdt_timer < 0)
+		return fdt_node_offset_by_compatible(_libkvmplat_dtb,
+						-1, "arm,armv7-timer");
 
+	return fdt_timer;
+}
+
+static uint32_t generic_timer_get_frequency(int fdt_timer)
+{
+	int len;
+	const uint64_t *fdt_freq;
+
+	/*
+	 * On a few platforms the frequency is not configured correctly
+	 * by the firmware. A property in the DT (clock-frequency) has
+	 * been introduced to workaround those firmware.
+	 */
 	fdt_freq = fdt_getprop(_libkvmplat_dtb,
-			fdt_archtimer, "clock-frequency", &len);
+			fdt_timer, "clock-frequency", &len);
 	if (!fdt_freq || (len <= 0)) {
 		uk_pr_info("No clock-frequency found, reading from register directly.\n");
 		goto endnofreq;
@@ -93,7 +188,35 @@ static uint32_t get_counter_frequency(void)
 	return fdt32_to_cpu(fdt_freq[0]);
 
 endnofreq:
+	/* No workaround, get from register directly */
 	return SYSREG_READ32(cntfrq_el0);
+}
+
+static uint32_t generic_timer_get_interrupts(int fdt_timer)
+{
+	int nintr, len;
+	const fdt32_t *regs;
+
+	nintr = fdt_interrupt_cells(_libkvmplat_dtb, fdt_timer);
+	if (nintr < 0 || nintr >= FDT_MAX_NCELLS)
+		UK_CRASH("Could not find proper interrupt cells!\n");
+
+	/* Otherwise, read from device tree */
+	regs = fdt_getprop(_libkvmplat_dtb,
+			fdt_timer, "interrupts", &len);
+	/*
+	 * Interrupt list for secure, non-secure, virtual and
+	 * hypervisor timers, in that order.
+	 */
+	if (regs == NULL || len < (int)sizeof(fdt32_t) * nintr)
+		UK_CRASH("Bad 'interrupts' property: %p %d\n", regs, nintr);
+
+	uk_pr_info("mini len:%d, nintr: %d\n", sizeof(fdt32_t) * (nintr * 4), nintr);
+	uk_pr_info("len:%d interrupts: %08x %08x %08x\n", len, fdt32_to_cpu(regs[0]), fdt32_to_cpu(regs[1]), fdt32_to_cpu(regs[2] ));
+	uk_pr_info("len:%d interrupts: %08x %08x %08x\n", len, fdt32_to_cpu(regs[3]), fdt32_to_cpu(regs[4]), fdt32_to_cpu(regs[5]));
+	uk_pr_info("len:%d interrupts: %08x %08x %08x\n", len, fdt32_to_cpu(regs[6]), fdt32_to_cpu(regs[7]), fdt32_to_cpu(regs[8]));
+	uk_pr_info("len:%d interrupts: %08x %08x %08x\n", len, fdt32_to_cpu(regs[9]), fdt32_to_cpu(regs[10]), fdt32_to_cpu(regs[11]));
+	return fdt32_to_cpu(regs[1]);
 }
 
 #ifdef CONFIG_ARM64_ERRATUM_858921
@@ -106,7 +229,7 @@ endnofreq:
  * the two read values. If bit[32] is different, keep the first value,
  * otherwise keep the second value.
  */
-static uint64_t read_virtual_count(void)
+static uint64_t generic_timer_get_ticks(void)
 {
     uint64_t val_1st, val_2nd;
 
@@ -115,7 +238,7 @@ static uint64_t read_virtual_count(void)
     return (((val_1st ^ val_2nd) >> 32) & 1) ? val_1st : val_2nd;
 }
 #else
-static inline uint64_t read_virtual_count(void)
+static inline uint64_t generic_timer_get_ticks(void)
 {
 	return SYSREG_READ64(cntvct_el0);
 }
@@ -127,49 +250,143 @@ static inline uint64_t read_virtual_count(void)
  */
 static __nsec generic_timer_monotonic(void)
 {
-	return (__nsec)ticks_to_ns(read_virtual_count() - boot_ticks);
+	return (__nsec)ticks_to_ns(generic_timer_get_ticks() - boot_ticks);
 }
 
 /*
  * Return epoch offset (wall time offset to monotonic clock start).
  */
-static __u64  generic_timer_epochoffset(void)
+static uint64_t generic_timer_epochoffset(void)
 {
 	return 0;
 }
 
+/*
+ * Returns early if any interrupts are serviced, or if the requested delay is
+ * too short. Must be called with interrupts disabled, will enable interrupts
+ * "atomically" during idle loop.
+ *
+ * This function must be called only from the scheduler. It will screw
+ * your system if you do otherwise. And, there is no reason you
+ * actually want to use it anywhere else. THIS IS NOT A YIELD or any
+ * kind of mutex_lock. It will simply halt the cpu, not allowing any
+ * other thread to execute.
+ */
+static void generic_timer_cpu_block(uint64_t until_ns)
+{
+	uint64_t now_ns, delta_ns;
+	uint64_t now_ticks, delta_ticks;
+
+	UK_ASSERT(ukplat_lcpu_irqs_disabled());
+
+	/* Record current ticks */
+	now_ticks = generic_timer_get_ticks();
+	now_ns = ticks_to_ns(now_ticks - boot_ticks);
+
+	/*
+	 * Compute delta in counter ticks. Return if it is less than minimum
+	 * safe amount of ticks. Essentially this will cause us to spin until
+	 * the timeout.
+	 */
+	delta_ns = until_ns - now_ns;
+	delta_ticks = ns_to_ticks(delta_ns);
+	if (delta_ticks < counter_mini_delta) {
+		/*
+		 * Since we are "spinning", quickly enable interrupts in
+		 * the hopes that we might get new work and can do something
+		 * else than spin.
+		 */
+		ukplat_lcpu_enable_irq();
+		nop();
+		ukplat_lcpu_disable_irq();
+
+		return;
+	}
+
+	/* Calculate the next match ticks for compare counter */
+	generic_timer_update_compare(now_ticks + delta_ticks);
+
+	/* Unmask the IRQ for next match interrupt */
+	generic_timer_unmask_irq();
+
+	/*
+	 * Wait for any interrupt. If we got an interrupt then just
+	 * return into the scheduler (this func is called _ONLY_ from
+	 * a scheduler, see the note above) which will check if there
+	 * is work to do and call us again here if not.
+	 *
+	 * TODO: It would be more efficient for longer sleeps to be
+	 * able to distinguish if the interrupt was the timer interrupt
+	 * and no other, but this will do for now.
+	 */
+	ukplat_lcpu_halt_irq();
+}
+
 static int generic_timer_init(void)
 {
-	/*
-	 * Calculate counter shift factor and scaling multiplier.
-	 *
-	 * counter_shift (S) needs to be the largest (<=32) shift factor where
-	 * the result of the ns_per_tick calculation below fits into uint32_t
-	 * without truncation. Note that we disallow an S of zero to ensure
-	 * the loop always terminates.
-	 *
-	 * (0.S) ns_per_tick = NSEC_PER_SEC (S.S) / counter_freq (S.0)
-	 */
-	uint64_t tmp;
-
-	counter_freq = get_counter_frequency();
-	counter_shift = 32;
-	do {
-		tmp = (NSEC_PER_SEC << counter_shift) / counter_freq;
-		if ((tmp & 0xFFFFFFFF00000000L) == 0L)
-			ns_per_tick = (uint32_t)tmp;
-		else
-			counter_shift--;
-	} while (counter_shift > 0 && ns_per_tick == 0L);
-	UK_BUGON(!ns_per_tick);
+	int fdt_timer;
 
 	/*
 	 * Monotonic time begins at boot_ticks (first read of counter
 	 * before calibration).
 	 */
-	boot_ticks = read_virtual_count();
+	boot_ticks = generic_timer_get_ticks();
+
+	/* Save timer fdt offset to get frequency and interrupts */
+	fdt_timer = generic_timer_get_fdt_offset();
+	if (fdt_timer < 0)
+		UK_CRASH("Failed device tree node for generic timer\n");
+
+	/* Get counter frequency from DTB or register */
+	counter_freq = generic_timer_get_frequency(fdt_timer);
+
+	/*
+	 * Calculate the shift factor and scaling multiplier for
+	 * cpnverting ticks to ns.
+	 */
+	calculate_mult_shift(&ns_per_tick, &counter_shift_to_ns,
+				counter_freq, NSEC_PER_SEC);
+
+	/* We disallow zero ns_per_tick */
+	UK_BUGON(!ns_per_tick);
+
+	/*
+	 * Calculate the shift factor and scaling multiplier for
+	 * cpnverting ns to ticks.
+	 */
+	calculate_mult_shift(&tick_per_ns, &counter_shift_to_tick,
+				NSEC_PER_SEC, counter_freq);
+
+	/* We disallow zero ns_per_tick */
+	UK_BUGON(!tick_per_ns);
+
+	/*
+	 * Set minimal counter delta, programming seems to have an overhead
+	 * of 3-4us, but play it safe here.
+	 */
+	counter_mini_delta = ns_to_ticks(4000);
+
+	uk_pr_info("counter_freq:0x%016x ns_per_tick:0x%08x counter_shift_to_ns: %d tick_per_ns:0x%08x counter_shift_to_tick:%d counter_mini_delta: 0x%08x\n",
+		counter_freq, ns_per_tick, counter_shift_to_ns,
+		tick_per_ns, counter_shift_to_tick, counter_mini_delta);
+
+	generic_timer_get_interrupts(fdt_timer);
 
 	return 0;
+}
+
+static int generic_timer_irq_handler(void *arg __unused)
+{
+	/*
+	 * We just mask the IRQ here, the scheduler will call
+	 * generic_timer_cpu_block, and then unmask the IRQ.
+	 */
+	generic_timer_mask_irq();
+
+	uk_pr_info("...timer_handler...\n");
+
+	/* Yes, we handled the irq. */
+	return 1;
 }
 
 unsigned long sched_have_pending_events;
@@ -177,11 +394,7 @@ unsigned long sched_have_pending_events;
 void time_block_until(__snsec until)
 {
 	while ((__snsec) ukplat_monotonic_clock() < until) {
-		/*
-		 * TODO:
-		 * As we haven't support interrupt on Arm, so we just
-		 * use busy polling for now.
-		 */
+		generic_timer_cpu_block(until);
 		if (__uk_test_and_clear_bit(0, &sched_have_pending_events))
 			break;
 	}
@@ -199,42 +412,26 @@ __nsec ukplat_clock_wall(void)
 	return generic_timer_monotonic() + generic_timer_epochoffset();
 }
 
-/* open timer */
-void ukplat_open_timer(void)
-{
-        SYSREG_WRITE32(CNTV_CTL_EL0, 1);
-	uk_printk("CNTV_CTL_EL0 is %d\n",SYSREG_READ32(CNTV_CTL_EL0));
-}
-
-/* close timer */
-void ukplat_close_timer(void)
-{
-        SYSREG_WRITE32(CNTV_CTL_EL0, 0);
-}
-
-/* set timer value */
-void ukplat_set_timer_value(uint32_t value)
-{
-        SYSREG_WRITE32(CNTV_TVAL_EL0, value);
-	uk_printk("CNTV_TVAL_EL0 is %d\n",SYSREG_READ32(CNTV_TVAL_EL0));
-}
-
-static int timer_handler(void *arg __unused)
-{
-	/* Yes, we handled the irq. */
-	return 1;
-}
-
 /* must be called before interrupts are enabled */
 void ukplat_time_init(void)
 {
 	int rc;
 
-	rc = ukplat_irq_register(27, timer_handler, NULL);
+	/* Register timer IRQ */
+	rc = ukplat_irq_register(27, generic_timer_irq_handler, NULL);
 	if (rc < 0)
 		UK_CRASH("Failed to register timer interrupt handler\n");
 
 	rc = generic_timer_init();
 	if (rc < 0)
 		UK_CRASH("Failed to initialize platform time\n");
+
+	/*
+	 * Mask IRQ before scheduler start working. Otherwise we will get
+	 * unexpected timer interrupts when system is booting.
+	 */
+	generic_timer_mask_irq();
+
+	/* Enable timer */
+	generic_timer_enable();
 }
