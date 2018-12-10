7abecf53875b8f6d56281dcb1e104473cb0f59d1
Wei Chen <wei.chen@arm.com>
2018-12-10 07:31:28 +0000

    plat/fdt: Remove redundant code for fdt_address/size_cells
    We use fdt_get_cells to reduce the redundant code.
    
    Change-Id: I92d50475be9811161b9b2d8a3ca692265a76024a
    Jira: ENTOS-860
    Signed-off-by: Wei Chen <wei.chen@arm.com>
    Signed-off-by: Jianyong Wu <jianyong.wu@arm.com>

--- lib/fdt/fdt_addresses.c
+++ lib/fdt/fdt_addresses.c
@@ -57,40 +57,10 @@
 
 int fdt_address_cells(const void *fdt, int nodeoffset)
 {
-	const fdt32_t *ac;
-	int val;
-	int len;
-
-	ac = fdt_getprop(fdt, nodeoffset, "#address-cells", &len);
-	if (!ac)
-		return 2;
-
-	if (len != sizeof(*ac))
-		return -FDT_ERR_BADNCELLS;
-
-	val = fdt32_to_cpu(*ac);
-	if ((val <= 0) || (val > FDT_MAX_NCELLS))
-		return -FDT_ERR_BADNCELLS;
-
-	return val;
+	return fdt_get_cells(fdt, "#address-cells", nodeoffset);
 }
 
 int fdt_size_cells(const void *fdt, int nodeoffset)
 {
-	const fdt32_t *sc;
-	int val;
-	int len;
-
-	sc = fdt_getprop(fdt, nodeoffset, "#size-cells", &len);
-	if (!sc)
-		return 2;
-
-	if (len != sizeof(*sc))
-		return -FDT_ERR_BADNCELLS;
-
-	val = fdt32_to_cpu(*sc);
-	if ((val < 0) || (val > FDT_MAX_NCELLS))
-		return -FDT_ERR_BADNCELLS;
-
-	return val;
+	return fdt_get_cells(fdt, "#size-cells", nodeoffset);
 }
